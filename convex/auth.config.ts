15a347bd92326d237c53a090c348d5254998f491
Innocent Ogayi <93614026+inno03@users.noreply.github.com>
2024-06-11 08:14:38 +0300

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://above-peacock-33.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
